95aed616e58a9ea43316ceb6f7fa632864a6bb43
tkmagesh <tkmagesh77@gmail.com>
2013-10-07 16:08:38 -0700

    Multithreading using Workers

---
+++ Day-1/WorkersDemo/DoWorkWorker.js
@@ -0,0 +1,16 @@
+function doWork(){
+	for(var i=0;i<1000;i++)
+		for(var j=0;j<1000;j++)
+			for(var k=0;k<100;k++)
+			{}
+}
+self.addEventListener("message",function(msgEvt){
+	if (msgEvt.data === "start"){
+		for(var i=0;i<100;i++){
+			doWork();
+			self.postMessage({type : "progress", percentCompleted : i + 1});	
+		}
+		
+		self.postMessage({type : "completed"});		
+	}
+})
